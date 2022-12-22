afd5e5e792dcde02a639485427937be568641043
Yiğit Erdinç <yigiterdinc@gmail.com>
2022-12-22 15:08:24 +0300

    Added MoviesModel

--- Movies and Series/Screens/Movies/Model/MoviesModel.swift
+++ Movies and Series/Screens/Movies/Model/MoviesModel.swift
@@ -14,6 +14,13 @@ struct MoviesModel: Codable {
 	var totalPages: Int?
 	var totalResults: Int?
 
+  init(page: Int? = nil, results: [MoviesResult]? = nil, totalPages: Int? = nil, totalResults: Int? = nil) {
+    self.page = page
+    self.results = results
+    self.totalPages = totalPages
+    self.totalResults = totalResults
+  }
+
 	enum CodingKeys: String, CodingKey {
 		case page = "page"
 		case results = "results"
